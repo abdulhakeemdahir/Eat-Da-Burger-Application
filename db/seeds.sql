07cc296e675c0018938dbde060b98c81ed6c3c3d
Abdulhakeem Dahir <somlutions@gmail.com>
2021-01-18 14:18:33 -0800

    setup seed db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, ate) VALUES ('Krusty Krab Burger', true);
+INSERT INTO burgers (name, ate) VALUES ('Chumbucket Burger', true);
