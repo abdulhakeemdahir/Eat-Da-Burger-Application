acff2bde2f227b94ecb6014a71da6683333049d1
Abdulhakeem Dahir <somlutions@gmail.com>
2021-01-18 14:13:19 -0800

    added connection js

--- config/connection.js
+++ config/connection.js
@@ -7,3 +7,12 @@ const connection = mysql.createConnection({
   database: "burger_db",
 });
 
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
